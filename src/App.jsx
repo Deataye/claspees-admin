98f2e909a3f92d390d15ee784da2112ed316229f
Deataye <shahzaiba61@gmail.com>
2025-02-03 15:08:47 +0500

    chheec

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import AdminPanel from "./Components/AdminPanel";
 
 function App() {
@@ -6,6 +6,8 @@ function App() {
     <Router>
       <div className="min-h-[calc(100vh-200px)]">
         <Routes>
+          {/* Add these 2 routes */}
+          <Route path="/" element={<Navigate to="/admin-panel" replace />} />
           <Route path="/admin-panel" element={<AdminPanel />} />
         </Routes>
       </div>
